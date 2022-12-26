a9a1bdf9becf2162d1d1567f59b9f007c33350fd
ranjitnadar <nranjithnadar@gmail.com>
2022-12-27 00:45:32 +0530

    3 files modified

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -11,7 +11,9 @@ export class ProfileComponent {
   users: any = [];
   user: any = null;
   userRepo: any = null;
+  searchHistory: any = {};
   searchText: String = null;
+  isHistoryActive: Boolean = false;
   displayUserDetailContainer: Boolean = false;
   displayGithubUserErrorNotFound: Boolean = false;
   searchContainerHeight: any = {};
@@ -23,15 +25,16 @@ export class ProfileComponent {
 
   ngOnInit(): void {
     this.setUserListHeight();
-    // this.getUsers();
   }
-  getUsers(search: any): void {
-    console.log(this.searchText);
+  getUsers(search: any = null): void {
     this.user = null;
-    if (search.length == 0) {
+    this.searchText = search;
+    this.isHistoryActive = false;
+    if (search == null || search?.length == 0) {
       this.users = [];
       this.setUserListHeight(); //set user list height
     } else {
+      this.searchHistory[search] = search;
       this.userservice.searchResults(search).subscribe((data: any) => {
         this.users = data;
         this.setUserListHeight(); //set user list height
@@ -42,21 +45,33 @@ export class ProfileComponent {
   getUserDetail(param: any): void {
     this.userservice.userDetails(param.login).subscribe((user) => {
       this.user = user;
-      console.log(this.user);
     });
     this.userservice.userRepoDetail(param.login).subscribe((repo) => {
       this.userRepo = repo;
     });
+  }
+  getHistoryList(): void {
+    this.isHistoryActive = true;
   }
   getSearchList(): void {
     this.user = null;
   }
   setUserListHeight(): void {
     let tempHeight = !this.users || this.users.length == 0 ? '60vh' : '30vh';
-    console.log(tempHeight, this.users);
     this.searchContainerHeight = {
-      // borderBottom: '3px solid lightgreen'
       ['height']: tempHeight,
     };
+  }
+  handleSeachHistoryHash(param): string[] {
+    return Object.keys(param);
+  }
+  removeHistory(param): void {
+    if (this.searchHistory.hasOwnProperty(param)) {
+      delete this.searchHistory[param];
+      this.searchText = null;
+    }
+    if (Object.keys(this.searchHistory).length == 0) {
+      this.getUsers();
+    }
   }
 }
