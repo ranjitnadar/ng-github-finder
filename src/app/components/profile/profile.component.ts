908815b60ea18652d2e2c20f23a427a3450e24ca
ranjitnadar <nranjithnadar@gmail.com>
2022-12-26 20:47:20 +0530

    7 files modified

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -9,19 +9,56 @@ import { User } from '../../models/user';
 })
 export class ProfileComponent {
   users: any;
-  searchText!: string;
-  displayUserDetailContainer = false;
-  displayGithubUserErrorNotFound = false;
+  user: any = null;
+  userRepo: any = null;
+  searchText: String = null;
+  displayUserDetailContainer: Boolean = false;
+  displayGithubUserErrorNotFound: Boolean = false;
+  searchContainerHeight = {
+    // borderBottom: '3px solid lightgreen'
+    ['height']: '100vh',
+  };
   constructor(private userservice: RepositoryUserService) {}
 
   //accessing form inputs
   @ViewChild('f')
   searchForm!: NgForm;
 
-  ngOnInit(): void {}
-  getUsers(search: any) {
-    this.userservice
-      .searchResults(search)
-      .subscribe((data: any) => (this.users = data));
+  ngOnInit(): void {
+    // this.getUsers();
+  }
+  getUsers(search: any): void {
+    this.searchText = search;
+    this.user = null;
+    if (search.length == 0) {
+      this.users = [];
+      this.setUserListHeight(); //set user list height
+    } else {
+      this.userservice.searchResults(search).subscribe((data: any) => {
+        this.users = data;
+        this.setUserListHeight(); //set user list height
+      });
+    }
+  }
+
+  getUserDetail(param: any): void {
+    this.userservice.userDetails(param.login).subscribe((user) => {
+      this.user = user;
+      console.log(this.user);
+    });
+    this.userservice.userRepoDetail(param.login).subscribe((repo) => {
+      this.userRepo = repo;
+    });
+  }
+  getSearchList(): void {
+    this.user = null;
+  }
+  setUserListHeight(): void {
+    let tempHeight = this.users.length == 0 ? '100vh' : '30vh';
+    console.log(tempHeight, this.users);
+    this.searchContainerHeight = {
+      // borderBottom: '3px solid lightgreen'
+      ['height']: tempHeight,
+    };
   }
 }
--- src/app/services/repository-user.service.ts
+++ src/app/services/repository-user.service.ts
@@ -2,30 +2,39 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { env } from '../../env/dev';
 import { Repository } from '../models/repository';
-// import { User } from '../models/user';
 import { UsersInterface } from '../interfaces/userInterface';
 import { map, Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root',
 })
 export class RepositoryUserService {
-  // getUserDetails: User;
   getRepositoryDetails: Repository;
   userListAPI = env.apiUrl;
   userDetailAPI = `${env.apiUrl}/users`;
   constructor(private http: HttpClient) {}
+  /**
+   * @name searchResults
+   * @description Search github profile using username
+   */
   searchResults(username: string): Observable<UsersInterface> {
     return this.http
       .get<UsersInterface>(`${this.userListAPI}/search/users?q=${username}`)
       .pipe(map((response: any) => response.items));
   }
-  // Get user details
+  /**
+   * @name userDetails
+   * @description Get github profile detail using username
+   */
   userDetails(username: string): Observable<UsersInterface> {
     return this.http.get<UsersInterface>(`${this.userDetailAPI}/${username}`);
   }
-
-  // Get single user
-  userDetail(username: string): Observable<UsersInterface> {
-    return this.http.get<UsersInterface>(`${this.userDetailAPI}/${username}`);
+  /**
+   * @name userRepoDetail
+   * @description Get github user repo detail using username
+   */
+  userRepoDetail(username: string): Observable<UsersInterface> {
+    return this.http.get<UsersInterface>(
+      `${this.userDetailAPI}/${username}/repos?per_page=15&sort=creted:asc`
+    );
   }
 }
